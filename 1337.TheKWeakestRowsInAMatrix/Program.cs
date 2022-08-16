ef88cbf0952d24497bc12790c0ecd700997f3595
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-16 16:32:29 -0300

    1337 and 1342

---
+++ 1337.TheKWeakestRowsInAMatrix/Program.cs
@@ -0,0 +1,74 @@
+﻿//Input: mat =
+//[[1, 1, 0, 0, 0],
+// [1,1,1,1,0],
+// [1,0,0,0,0],
+// [1,1,0,0,0],
+// [1,1,1,1,1]], 
+//k = 3
+//Output: [2,0,3]
+int[][] mat = new int[][] {
+    new int[] { 1, 1, 0, 0, 0 },
+    new int[] { 1, 1, 1, 1, 0 },
+    new int[] { 1, 0, 0, 0, 0 },
+    new int[] { 1, 1, 0, 0, 0 },
+    new int[] { 1, 1, 1, 1, 1 }};
+Array.ForEach(new Solution().KWeakestRows(mat, 3), Console.WriteLine);
+Console.WriteLine("");
+
+//Input: mat =
+//[[1, 0, 0, 0],
+// [1,1,1,1],
+// [1,0,0,0],
+// [1,0,0,0]], 
+//k = 2
+//Output: [0,2]
+int[][] mat2 = new int[][] {
+    new int[] {1, 0, 0, 0 },
+    new int[] { 1,1,1,1 },
+    new int[] { 1,0,0,0 },
+    new int[] { 1,0,0,0 }};
+Array.ForEach(new Solution().KWeakestRows(mat2, 2), Console.WriteLine);
+Console.WriteLine("");
+
+//[[1,0],[1,0],[1,0],[1,1]]
+//4
+int[][] mat3 = new int[][] {
+    new int[] {1,0 },
+    new int[] { 1,0 },
+    new int[] { 1,0 },
+    new int[] { 1,1 }};
+Array.ForEach(new Solution().KWeakestRows(mat3, 4), Console.WriteLine);
+Console.WriteLine("");
+
+public class Solution
+{
+    public int[] KWeakestRows(int[][] mat, int k)
+    {
+        var rows = mat.Length;
+        var columns = mat[0].Length;
+        int[] kCount = new int[rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (mat[i][j] == 0)
+                    break;
+                kCount[i] += 1;
+            }
+
+            kCount[i] = (kCount[i] * rows) + i;
+        }
+
+        Array.Sort(kCount);
+        int[] answer = new int[k];
+
+        for (int i = 0; i < k; i++)
+        {
+            answer[i] = kCount[i] % rows;
+        }
+
+        return answer;
+
+    }
+}
---
+++ 1342.NumberOfStepsToReduceANumberToZero/Program.cs
@@ -0,0 +1,33 @@
+﻿//Input: num = 14
+//Output: 6
+Console.WriteLine(new Solution().NumberOfSteps(14));
+
+//Input: num = 8
+//Output: 4
+Console.WriteLine(new Solution().NumberOfSteps(8));
+
+//Input: num = 123
+//Output: 12
+Console.WriteLine(new Solution().NumberOfSteps(123));
+
+//Input: num = 0
+//Output: 0
+Console.WriteLine(new Solution().NumberOfSteps(0));
+
+public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        var count = 0;
+        while (num > 0)
+        {
+            count++;
+            if (num % 2 == 0)
+                num = num / 2;
+            else
+                num = num - 1;
+        }
+
+        return count;
+    }
+}
