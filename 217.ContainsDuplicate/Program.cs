16ba58609168b7847466e7ad2a343dd85ed64890
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-18 03:43:28 -0300

    add 217

---
+++ 217.ContainsDuplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿//Input: nums = [1, 2, 3, 1]
+//Output: true
+Console.WriteLine(new Solution().ContainsDuplicate(new int[] {1,2,3,1}));
+
+//Input: nums = [1, 2, 3, 4]
+//Output: false
+Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1,2,3,4 }));
+
+//Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//Output: true
+Console.WriteLine(new Solution().ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+                return true;
+
+            dic.Add(nums[i], i);
+        }
+
+        return false;
+    }
+}
