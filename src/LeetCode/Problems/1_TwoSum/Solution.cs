a13c96b3e7504ae6dad5226302b4076af4de86df
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2025-12-31 16:37:35 -0300

    Add Two Sum

---
+++ src/LeetCode/Problems/1_TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems._1_TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for(var i = 0; i < nums.Length; i++)
+            {
+                var missing = target - nums[i];
+
+                if(dictionary.ContainsKey(missing))
+                    return new int[] { dictionary[missing], i };
+
+                dictionary[nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/Problems/1_TwoSum/TwoSumTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Problems._1_TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.Problems._1_TwoSum
+{
+    public class TwoSumTests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] {0, 1})]
+        [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+        [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+        public void TwoSum_Test(int[] nums, int target, int[] expected)
+        {
+            var solution = new Solution();
+
+            var result = solution.TwoSum(nums, target);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
