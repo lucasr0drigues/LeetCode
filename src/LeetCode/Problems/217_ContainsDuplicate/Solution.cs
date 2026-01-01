4f493d9563a33e6bce841b4b758825c378e660aa
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-01 13:19:00 -0300

    Add 217 Solution

--- src/LeetCode/Problems/217_ContainsDuplicate/Solution.cs
+++ src/LeetCode/Problems/217_ContainsDuplicate/Solution.cs
@@ -6,6 +6,19 @@ namespace LeetCode.Problems._217_ContainsDuplicate
 {
     public class Solution
     {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
 
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.ContainsKey(nums[i]))
+                    return true;
+
+                dictionary[nums[i]] = i;
+            }
+
+            return false;
+        }
     }
 }
