d54a0690260b4d2cb91f91ab2bc670a60678ec22
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-02 11:35:59 -0300

    Add 242

---
+++ src/LeetCode/Problems/242_ValidAnagram/242_ValidaAnagramSolution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems._242_ValidAnagram
+{
+    internal class _242_ValidaAnagramSolution
+    {
+    }
+}
