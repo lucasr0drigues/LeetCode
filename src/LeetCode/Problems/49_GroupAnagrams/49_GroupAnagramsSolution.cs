445054a12797887b704f30a404b447e7fb89a95a
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-03 19:52:36 -0300

    Add 49

---
+++ src/LeetCode/Problems/49_GroupAnagrams/49_GroupAnagramsSolution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems._49_GroupAnagrams
+{
+    internal class _49_GroupAnagramsSolution
+    {
+    }
+}
