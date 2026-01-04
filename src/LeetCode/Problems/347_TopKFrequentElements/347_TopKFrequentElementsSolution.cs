f57d9cbf3f80daaae0b994e50218a2efd7602adf
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-04 19:02:27 -0300

    Add 347 Solution

--- src/LeetCode/Problems/347_TopKFrequentElements/347_TopKFrequentElementsSolution.cs
+++ src/LeetCode/Problems/347_TopKFrequentElements/347_TopKFrequentElementsSolution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LeetCode.Problems._347_TopKFrequentElements
@@ -10,6 +11,43 @@ namespace LeetCode.Problems._347_TopKFrequentElements
         {
             if(nums.Length == 1)
                 return nums;
+
+            var count = new Dictionary<int, int>();
+            List<int>[] freq = new List<int>[nums.Length +1];
+
+            for(int i = 0; i < freq.Length; i++)
+            {
+                freq[i] = new List<int>();
+            }
+
+            foreach(var num in nums)
+            {
+                if (count.ContainsKey(num))
+                    count[num]++;
+                else
+                    count[num] = 1;
+            }
+
+            foreach(var entry in count)
+            {
+                freq[entry.Value].Add(entry.Key);
+            }
+
+            var result = new int[k];
+
+            int index = 0;
+            for (int i = freq.Length - 1; i > 0 && index < k; i--)
+            {
+                foreach (int n in freq[i])
+                {
+                    result[index++] = n;
+                    if (index == k)
+                    {
+                        return result;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
