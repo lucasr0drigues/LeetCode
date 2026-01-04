9a400df233d1c5f505ca2a469e453b5a48cc53b7
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-04 13:04:28 -0300

    Add 49 Solution and Tests

--- src/LeetCode/Problems/49_GroupAnagrams/49_GroupAnagramsSolution.cs
+++ src/LeetCode/Problems/49_GroupAnagrams/49_GroupAnagramsSolution.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace LeetCode.Problems._49_GroupAnagrams
 {
-    internal class _49_GroupAnagramsSolution
+    public class _49_GroupAnagramsSolution
     {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            if(strs.Length <= 1)
+                return new List<IList<string>>() { new List<string>() { strs.FirstOrDefault()! } };
+
+            var result = new Dictionary<string, IList<string>>();
+
+            foreach(var word in strs)
+            {
+                int[] count = new int[26];
+                //char[] count = new char[26]; // use char array to have a lower runtime
+
+                foreach (var letter in word)
+                {
+                    count[letter - 'a'] += 1;
+                    //count[letter - 'a']++;
+                }
+
+                var key = string.Join(" ", count);
+                //string key = new string(count);
+
+                if (!result.ContainsKey(key))
+                    result[key] = new List<string>();
+
+                result[key].Add(word);
+            }
+
+            return result.Values.ToList();
+        }
     }
 }
---
+++ tests/LeetCode.Tests/Problems/49_GroupAnagrams/49_GroupAnagramsTests.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Problems._49_GroupAnagrams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests.Problems._49_GroupAnagrams
+{
+    public class _49_GroupAnagramsTests
+    {
+        public static IEnumerable<object[]> GroupAnagramsCases =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new[] { "act", "pots", "tops", "cat", "stop", "hat" },
+                new[]{new[] { "hat" },new[] { "act", "cat" }, new[] { "stop", "pots", "tops" }}
+            },
+            new object[]
+            {
+                new[] { "x" },
+                new[]{ new[] { "x" }}
+            },
+            new object[]
+            {
+                new[] { "" },
+                new[]{ new[] { "" }}
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(GroupAnagramsCases))]
+        public void GroupAnagrams_Test(string[] input, string[][] output)
+        {
+            var solution = new _49_GroupAnagramsSolution();
+
+            var result = solution.GroupAnagrams(input);
+
+            var orderedOutput = output
+                .Select(g => g.OrderBy(x => x).ToArray())
+                .OrderBy(g => g.Length)
+                .ThenBy(g => string.Join(",", g))
+                .ToArray();
+
+            var orderedResult = result
+                .Select(g => g.OrderBy(x => x).ToArray())
+                .OrderBy(g => g.Length)
+                .ThenBy(g => string.Join(",", g))
+                .ToArray();
+
+            Assert.Equal(orderedOutput, orderedResult);
+        }
+    }
+}
