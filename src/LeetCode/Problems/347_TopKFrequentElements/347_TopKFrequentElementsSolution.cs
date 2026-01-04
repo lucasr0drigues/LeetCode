f2cc85af882add6932b98ca95bc70fb1df6362da
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2026-01-04 13:18:40 -0300

    Add 347

---
+++ src/LeetCode/Problems/347_TopKFrequentElements/347_TopKFrequentElementsSolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems._347_TopKFrequentElements
+{
+    public class _347_TopKFrequentElementsSolution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            if(nums.Length == 1)
+                return nums;
+        }
+    }
+}
